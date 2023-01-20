c1958f35e6d12df0902341b28e7703773ed217e4
Ana Beatriz de Almeida <97922588+anabeatrizalmeida@users.noreply.github.com>
2023-01-20 14:13:24 -0300

    implement peers

--- client/src/context/RoomContext.tsx
+++ client/src/context/RoomContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, useEffect } from "react";
+import Peer from "peerjs";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import socketIOClient from "socket.io-client";
+import { v4 as uuidV4 } from "uuid";
 
 const WS = "http://localhost:8080";
 const ws = socketIOClient(WS);
@@ -13,13 +15,37 @@ interface Props {
 
 export const RoomProvider: React.FunctionComponent<Props> = ({ children }) => {
   const navigate = useNavigate();
+  const [me, setMe] = useState<Peer>();
+  const [stream, setStream] = useState<MediaStream>();
+
   const enterRoom = ({ roomId }: { roomId: "string" }) => {
     console.log({ roomId });
     navigate(`/room/${roomId}`);
   };
+  const getUsers = ({ participants }: { participants: string[] }) => {
+    console.log({ participants });
+  };
 
   useEffect(() => {
+    const meId = uuidV4();
+
+    const peer = new Peer(meId);
+    setMe(peer);
+
+    try {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          setStream(stream);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+
     ws.on("room-created", enterRoom);
+    ws.on("get-users", getUsers);
   }, []);
-  return <RoomContext.Provider value={{ ws }}>{children}</RoomContext.Provider>;
+  return (
+    <RoomContext.Provider value={{ ws, me }}>{children}</RoomContext.Provider>
+  );
 };
